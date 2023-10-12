055ce17b3b90c6dc256b03a5345844041ecb3ab4
juhyunju <2juhyunju@naver.com>
2023-10-12 20:37:48 +0900

    feat: api 추가 및 리펙토링

--- app.js
+++ app.js
@@ -21,10 +21,24 @@ app.get('/',(req,res) => {
         message: "hello"
     })
 })
+// 유저 체크
+const checkUser = async (user_id) =>{
+    const result = await myDataSource.query(`
+    SELECT *
+        FROM users u
+        LEFT JOIN threads t ON u.id = t.user_id
+        WHERE u.id = ?
+    `,[user_id])
+    if(result.length !==null ){
+        console.log("result2222",result)
+        return result[0].id
+    }else {
+        return false
+    }
+}
 // 회원가입
 const signUp = async(req,res) =>{
     const {nickname, email, password} = req.body
-
     await myDataSource.query(`
     INSERT INTO users(
         nickname,email,password
@@ -34,6 +48,7 @@ const signUp = async(req,res) =>{
     `)
     res.status(201).json({message: "userCreated"})
 }
+// 쓰레드 생성
 const thread = async(req,res) =>{
     const {user_id, content} = req.body
     
@@ -45,7 +60,7 @@ const thread = async(req,res) =>{
         res.status(200).json({message: "postCreated"})
     }
 
-
+// 전체 쓰레드 조회
 const getThreads = async(req,res) =>{
     const threads = await myDataSource.query(`
     SELECT * FROM threads
@@ -59,19 +74,62 @@ const getThreads = async(req,res) =>{
 //     console.log(threads)
 //     res.status(200).json([threads])
 // })
+
+// 유저 쓰레드 조회
 const getThread = async(req,res) =>{
     const userId = req.params.userId
-    console.log("aa",userId)
+
     const threads = await myDataSource.query(`
     SELECT * FROM threads WHERE user_id = ?
     `,[userId])
     res.status(200).json(threads)
 }
+// 쓰레드 수정
+const updateThread = async(req,res) =>{
+    const {content,user_id,thread_id} = req.body
+    const user = await checkUser(user_id)
+    console.log(user)
+    if(user == thread_id){
+        await myDataSource.query(`
+        UPDATE threads SET content = '${content}', updated_at = NOW() WHERE id = '${thread_id}' AND  user_id = '${user_id}'
+        `)
+        res.status(200).json({message: "postUpdated"})
+    }else{
+        res.status(404).json({error: "User not found"})
+    }
+}
+// 쓰레드 삭제
+const deleteThread = async (req,res) =>{
+    const {user_id, thread_id} = req.body
+    const user = await checkUser(user_id)
+    console.log("req 하는 threadId",typeof(thread_id))
+    console.log("userId에 있는 thread ID",typeof(user))
+    if(user == thread_id){
+        await myDataSource.query(`
+        DELETE FROM threads WHERE user_id = '${user_id}' AND id = '${thread_id}'
+        `)
+        res.status(200).json({message: "postingDeleted"})
+    }
+    else {
+        res.status(404).json({error: "User not found"})
+    }
+}
+// 쓰레드 좋아요
+const threadLike = async (req,res) => {
+    const { user_id, thread_id } = req.body
+        await myDataSource.query(`
+        INSERT INTO thread_likes (user_id,thread_id) VALUES('${user_id}','${thread_id}')
+        `)
+        res.status(200).json({message: "likeCreated"})
+    }
 
 app.post("/users/sign-up", signUp)
 app.post("/threads/thread",thread)
 app.get("/threads",getThreads)
 app.get("/thread/:userId",getThread)
+app.put("/thread/update",updateThread)
+app.delete("/thread/delete",deleteThread)
+app.post("/thread/like",threadLike)
 const server = http.createServer(app)
 
 myDataSource.initialize()
