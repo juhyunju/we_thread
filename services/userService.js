f9cd3f631ae7cea92b8ebd025ad8bf7269189190
juhyunju <2juhyunju@naver.com>
2023-10-17 19:58:33 +0900

    feat: add email regex

--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const userDao = require('../models/userDao')
+const userDao = require('../models/userDao') 
 
 // 유저 체크
 const checkUser = async (user_id) =>{
@@ -22,11 +22,23 @@ const checkUser = async (user_id) =>{
 const signUp = async (nickname, email, password) => {
     // password validation using REGEX
     const pwValidation = new RegExp(
-      '^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})'
+      '^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{10,20})'
     );
     if (!pwValidation.test(password)) {
       const err = new Error('PASSWORD_IS_NOT_VALID');
       err.statusCode = 409;
+      throw err;
+    }
+    const checkEmail = await userDao.login(email)
+    if(checkEmail){
+      const err = new Error('이미 가입됨')
+      err.statusCode = 400
+      throw err;
+    }
+    const emailRegex = new RegExp(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+\.[A-Za-z]{2,}$/)
+    if(!emailRegex.test(email)){
+      const err = new Error('이메일 형식이 올바르지 않습니다.')
+      err.statusCode = 400
       throw err;
     }
       const createUser = await userDao.signUp(
@@ -53,8 +65,9 @@ const login = async(email,password) => {
       err.statusCode = 400
       throw err
     }
+
     const token = jwt.sign({userId: user.id, email: user.email},'key',{expiresIn: '1h'})
-    console.log(token)
+    console.log("tokenn",token)
     return {token,userId: user.id, email: user.email}
 
   }catch(err){
